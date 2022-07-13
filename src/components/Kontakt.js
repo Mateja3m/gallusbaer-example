01457c6e05940f2819dcbb7f980617b21b657e4b
Milan <milanmatejicdev@gmail.comn>
2022-07-13 17:47:52 +0200

    minor style improvements

---
+++ src/components/Kontakt.js
@@ -0,0 +1,13 @@
+import { Container, Row, Col } from "react-bootstrap";
+import "./Kontakt.css";
+const Kontakt = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Kontakt;
